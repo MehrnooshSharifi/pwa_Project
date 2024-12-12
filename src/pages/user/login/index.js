f0c5df7074a97cd91355e7c26608cf992db06b14
parisajahanian <p.jahaniiian@gmail.com>
2024-12-12 23:43:58 +0330

    .init

--- src/pages/user/login/index.js
+++ src/pages/user/login/index.js
@@ -7,8 +7,11 @@ import { useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import * as Yup from "yup";
 import Cookies from "js-cookie";
+import OTPPage from "../otp";
 export default function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
+  const [showOtpPage, setShowOtpPage] = useState(false);
+  const [otp, setOtp] = useState("");
   const router = useRouter();
   const initialValues = {
     id: "",
@@ -27,21 +30,33 @@ export default function LoginPage() {
       .max(11, "شماره تلفن همراه باید 11 رقم باشد"),
   });
   const onSubmit = async (user, { resetForm }) => {
-    console.log(user);
-    setIsLoading(true);
-    router.push("/user/otp");
-    const res = await Login(user);
-    if (res.isSuccess) {
-      console.log(res);
-      Cookies.set("Scope", res.Scope);
-      Cookies.set("phoneNumber", user.phoneNumber);
-      Cookies.set("nationalCode", user.id);
-      Cookies.set("userTypeId", user.userTypeId);
-      const userInfo = await GetUserInfo(user.id);
-      console.log(userInfo);
-    } else {
-      console.log(res.message);
-    }
+    try {
+      console.log(user);
+      setShowOtpPage(true);
+      resetForm();
+      const res = await Login(user);
+      if (res.isSuccess) {
+        Cookies.set("Scope", res.Scope);
+        Cookies.set("nationalCode", user.id);
+        Cookies.set("phoneNumber", user.phoneNumber);
+        Cookies.set("userTypeId", user.userTypeId);
+        if ("OTPCredential" in window) {
+          console.log("testssss");
+          try {
+            const otpCredential = await navigator.credentials.get({
+              otp: { transport: ["sms"] },
+            });
+            console.log("OTP Credential:", otpCredential); // Log for debugging
+            if (otpCredential && otpCredential.code) {
+              setOtp(otpCredential.code); // Autofill the OTP
+              otpInputRef.current?.blur(); // Optionally blur the input
+            }
+          } catch (err) {
+            console.error("Error fetching OTP:", err);
+          }
+        }
+      }
+    } catch (error) {}
   };
   const formik = useFormik({
     initialValues,
@@ -49,139 +64,154 @@ export default function LoginPage() {
     validationSchema,
     validateOnMount: true,
   });
+  const user = formik.values;
   return (
     <div className="flex flex-col container mx-auto bg-neutralColor-5 max-w-[600px] overflow-y-auto h-screen">
-      <form
-        onSubmit={formik.handleSubmit}
-        className="flex-col items-center  mt-[100px] "
-        autoComplete="off"
-      >
-        <div className=" flex flex-col items-center gap-y-[30px] ">
-          <div onClick={() => router.push("/")} className="cursor-pointer">
-            <Image
-              alt="logo"
-              src="/assets/Images/logo/newLogo.webp"
-              width={80}
-              height={80}
-            />
-          </div>
-          <h2 className="text-[16px] font-bold">ورود | ثبت نام</h2>
-          <div
-            className="flex gap-x-[10px] items-center
+      {!showOtpPage ? (
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex-col items-center  mt-[100px] "
+          autoComplete="off"
+        >
+          <div className=" flex flex-col items-center gap-y-[30px] ">
+            <div onClick={() => router.push("/")} className="cursor-pointer">
+              <Image
+                alt="logo"
+                src="/assets/Images/logo/newLogo.webp"
+                width={80}
+                height={80}
+              />
+            </div>
+            <h2 className="text-[16px] font-bold">ورود | ثبت نام</h2>
+            <div
+              className="flex gap-x-[10px] items-center
         "
-          >
-            <input
-              defaultChecked
-              className=""
-              type="radio"
-              id="genuine"
-              name="userTypeId"
-              value="0"
-              onChange={formik.handleChange}
-            />
-            <label for="genuine" className="text-[14px]">
-              حقیقی
-            </label>
-            <br />
-            <input
-              type="radio"
-              id="legal"
-              name="userTypeId"
-              value="1"
-              onChange={formik.handleChange}
-            />
-            <label for="legal" className="text-[14px]">
-              حقوقی
-            </label>
-            <br />
-          </div>
-          <div className="flex flex-col w-full max-w-[340px]">
-            <label
-              className={`${
-                formik.values.userTypeId == "0"
-                  ? "w-[56px]  h-[15px]"
-                  : "w-[80px]  h-[15px]"
-              }  text-neutralColor-2 flex items-center  text-[14px] lg:text-[14px] px-2  whitespace-nowrap leading-[27.64px]  bg-naturalColor-2 absolute -mt-[10px] mx-4  font-medium`}
             >
-              {formik.values.userTypeId == "0" ? "کدملی" : "شناسه ملی"}
-            </label>
-            <input
-              {...formik.getFieldProps("id")}
-              className="focus-within:border-2 w-full focus-within:border-primaryColor-1 pr-[6px]  h-[48px]  lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
-              name="id"
-              type="text"
-            />
-            <div className={`absolute top-[360px]  lg:text-[12px] text-[10px]`}>
-              {formik.touched.id && formik.errors.id && (
-                <div className="flex gap-x-2 items-center ">
-                  <span className="text-red-400">{formik.errors.id}</span>
-                </div>
-              )}
+              <input
+                defaultChecked
+                className=""
+                type="radio"
+                id="genuine"
+                name="userTypeId"
+                value="0"
+                onChange={formik.handleChange}
+              />
+              <label for="genuine" className="text-[14px]">
+                حقیقی
+              </label>
+              <br />
+              <input
+                type="radio"
+                id="legal"
+                name="userTypeId"
+                value="1"
+                onChange={formik.handleChange}
+              />
+              <label for="legal" className="text-[14px]">
+                حقوقی
+              </label>
+              <br />
             </div>
-          </div>
-          <div className="flex flex-col w-full max-w-[340px]">
-            <label className="w-[120px]  h-[15px]  text-neutralColor-2 flex items-center  text-[14px] lg:text-[14px] px-2  whitespace-nowrap leading-[27.64px]  bg-naturalColor-2 absolute -mt-[10px] mx-4  font-medium">
-              شماره تلفن همراه
-            </label>
-            <input
-              {...formik.getFieldProps("phoneNumber")}
-              className="focus-within:border-2 w-full focus-within:border-primaryColor-1 pr-[6px]  h-[48px]  lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
-              name="phoneNumber"
-              type="text"
-            />
-            <div className={`absolute top-[440px] lg:text-[12px] text-[10px]`}>
-              {formik.touched.phoneNumber && formik.errors.phoneNumber && (
-                <div className="flex gap-x-2 items-center ">
-                  <span className="text-red-400">
-                    {formik.errors.phoneNumber}
-                  </span>
-                </div>
-              )}
-            </div>
-          </div>
-          <button
-            disabled={!formik.isValid}
-            type="submit"
-            className={` rounded-[5px]  bg-errorColor-2  text-center  px-[16px] text-naturalColor-2 text-[14px] font-medium  w-full max-w-[340px] h-[48px]  lg:text-[16px]  bottom-[30px] lg:mb-2  mt-[17.51px] ${
-              !formik.isValid
-                ? "cursor-not-allowed opacity-30"
-                : "cursor-pointeropacity-100"
-            }`}
-          >
-            <div className="flex  justify-center items-center">
-              <div className="w-[40px] ">
-                {isLoading ? (
-                  <ThreeDots
-                    height="40"
-                    width="40"
-                    radius="9"
-                    color="#FAFAFA"
-                    ariaLabel="three-dots-loading"
-                    wrapperStyle={{}}
-                    wrapperClassName=""
-                    visible={true}
-                  />
-                ) : (
-                  <span className="whitespace-nowrap flex items-center justify-center">
-                    ورود | ثبت نام
-                  </span>
+            <div className="flex flex-col w-full max-w-[340px]">
+              <label
+                className={`${
+                  formik.values.userTypeId == "0"
+                    ? "w-[56px]  h-[15px]"
+                    : "w-[80px]  h-[15px]"
+                }  text-neutralColor-2 flex items-center  text-[14px] lg:text-[14px] px-2  whitespace-nowrap leading-[27.64px]  bg-naturalColor-2 absolute -mt-[10px] mx-4  font-medium`}
+              >
+                {formik.values.userTypeId == "0" ? "کدملی" : "شناسه ملی"}
+              </label>
+              <input
+                {...formik.getFieldProps("id")}
+                className="focus-within:border-2 w-full focus-within:border-primaryColor-1 pr-[6px]  h-[48px]  lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
+                name="id"
+                type="text"
+              />
+              <div
+                className={`absolute top-[360px]  lg:text-[12px] text-[10px]`}
+              >
+                {formik.touched.id && formik.errors.id && (
+                  <div className="flex gap-x-2 items-center ">
+                    <span className="text-red-400">{formik.errors.id}</span>
+                  </div>
                 )}
               </div>
             </div>
-          </button>
-          <div className="flex gap-x-[5px] text-[12px] w-full max-w-[340px]">
-            <span>ورود شما به معنای پذیرش</span>
-            <Link href="/" className="text-primaryColor-1">
-              شرایط تبادل
-            </Link>
-            <span>و</span>
-            <Link href="/" className="text-primaryColor-1">
-              قوانین حریم خصوصی
-            </Link>
-            <span>است</span>
+            <div className="flex flex-col w-full max-w-[340px]">
+              <label className="w-[120px]  h-[15px]  text-neutralColor-2 flex items-center  text-[14px] lg:text-[14px] px-2  whitespace-nowrap leading-[27.64px]  bg-naturalColor-2 absolute -mt-[10px] mx-4  font-medium">
+                شماره تلفن همراه
+              </label>
+              <input
+                {...formik.getFieldProps("phoneNumber")}
+                className="focus-within:border-2 w-full focus-within:border-primaryColor-1 pr-[6px]  h-[48px]  lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
+                name="phoneNumber"
+                type="text"
+              />
+              <div
+                className={`absolute top-[440px] lg:text-[12px] text-[10px]`}
+              >
+                {formik.touched.phoneNumber && formik.errors.phoneNumber && (
+                  <div className="flex gap-x-2 items-center ">
+                    <span className="text-red-400">
+                      {formik.errors.phoneNumber}
+                    </span>
+                  </div>
+                )}
+              </div>
+            </div>
+            <button
+              disabled={!formik.isValid}
+              type="submit"
+              className={` rounded-[5px]  bg-errorColor-2  text-center  px-[16px] text-naturalColor-2 text-[14px] font-medium  w-full max-w-[340px] h-[48px]  lg:text-[16px]  bottom-[30px] lg:mb-2  mt-[17.51px] ${
+                !formik.isValid
+                  ? "cursor-not-allowed opacity-30"
+                  : "cursor-pointeropacity-100"
+              }`}
+            >
+              <div className="flex  justify-center items-center">
+                <div className="w-[40px] ">
+                  {isLoading ? (
+                    <ThreeDots
+                      height="40"
+                      width="40"
+                      radius="9"
+                      color="#FAFAFA"
+                      ariaLabel="three-dots-loading"
+                      wrapperStyle={{}}
+                      wrapperClassName=""
+                      visible={true}
+                    />
+                  ) : (
+                    <span className="whitespace-nowrap flex items-center justify-center">
+                      ورود | ثبت نام
+                    </span>
+                  )}
+                </div>
+              </div>
+            </button>
+            <div className="flex gap-x-[5px] text-[12px] w-full max-w-[340px]">
+              <span>ورود شما به معنای پذیرش</span>
+              <Link href="/" className="text-primaryColor-1">
+                شرایط تبادل
+              </Link>
+              <span>و</span>
+              <Link href="/" className="text-primaryColor-1">
+                قوانین حریم خصوصی
+              </Link>
+              <span>است</span>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      ) : (
+        <OTPPage
+          setShowOtpPage={setShowOtpPage}
+          formik={formik}
+          otp={otp}
+          setOtp={setOtp}
+          user={user}
+        />
+      )}
     </div>
   );
 }
--- src/pages/user/otp/index.js
+++ src/pages/user/otp/index.js
@@ -4,102 +4,72 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { ThreeDots } from "react-loader-spinner";
 import { useCookies } from "react-cookie";
-import Cookies from "js-cookie";
-export default function OTPPage() {
-  const [cookies] = useCookies();
+export default function OTPPage({ setShowOtpPage, formik, otp, setOtp, user }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [otp, setOtp] = useState("");
   const otpInputRef = useRef(null); // Ref for the OTP input
   const router = useRouter();
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(`Entered OTP: ${otp}`);
   };
-
-  const handleOTPInput = async () => {
-    if ("OTPCredential" in window) {
-      try {
-        const otpCredential = await navigator.credentials.get({
-          otp: { transport: ["sms"] },
-        });
-        console.log("OTP Credential:", otpCredential); // Log for debugging
-        if (otpCredential && otpCredential.code) {
-          setOtp(otpCredential.code); // Autofill the OTP
-          otpInputRef.current?.blur(); // Optionally blur the input
-        }
-      } catch (err) {
-        console.error("Error fetching OTP:", err);
-      }
-    }
-  };
-
   useEffect(() => {
+    console.log(user);
     otpInputRef.current?.focus(); // Focus input on mount
-    handleOTPInput(); // Start listening for OTP autofill
   }, []);
-
   return (
-    <div className="flex flex-col container mx-auto bg-neutralColor-5 max-w-[600px] overflow-y-auto h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="flex-col items-center mt-[100px]"
-        autoComplete="off"
-      >
-        <div className="flex flex-col items-center gap-y-[30px]">
-          <div onClick={() => router.push("/")} className="cursor-pointer">
-            <Image
-              alt="logo"
-              src="/assets/Images/logo/newLogo.webp"
-              width={80}
-              height={80}
-            />
-          </div>
-          <h2 className="text-[16px] font-bold">کد تایید را وارد کنید</h2>
-          <p className="text-[14px] text-neutralColor-2">
-            کد تایید برای شماره {cookies.phoneNumber} پیامک شد
-          </p>
-          <div className="flex flex-col w-full max-w-[340px]">
-            <input
-              ref={otpInputRef}
-              className="focus-within:border-2 text-neutralColor-2 w-full focus-within:border-primaryColor-1 pr-[6px] h-[48px] lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
-              name="otp"
-              type="text"
-              onChange={(e) => setOtp(e.target.value)}
-              value={otp}
-              placeholder="Enter OTP"
-              autoFocus // Ensures field is focused
-            />
-          </div>
-          <button
-            disabled={!otp}
-            type="submit"
-            className={`rounded-[5px] bg-errorColor-2 text-center px-[16px] text-naturalColor-2 text-[14px] font-medium w-full max-w-[340px] h-[48px] ${
-              !otp
-                ? "cursor-not-allowed opacity-30"
-                : "cursor-pointer opacity-100"
-            }`}
-          >
-            {isLoading ? (
-              <ThreeDots
-                height="40"
-                width="40"
-                radius="9"
-                color="#FAFAFA"
-                ariaLabel="three-dots-loading"
-              />
-            ) : (
-              "تایید"
-            )}
-          </button>
-          <Link
-            href="/user/login"
-            className="text-[12px] font-bold text-primaryColor-1"
-          >
-            برگشت به مرحله قبل
-          </Link>
+    <form
+      onSubmit={handleSubmit}
+      className="flex-col items-center mt-[100px]"
+      autoComplete="off"
+    >
+      <div className="flex flex-col items-center gap-y-[30px]">
+        <div onClick={() => router.push("/")} className="cursor-pointer">
+          <Image
+            alt="logo"
+            src="/assets/Images/logo/newLogo.webp"
+            width={80}
+            height={80}
+          />
         </div>
-      </form>
-    </div>
+        <h2 className="text-[16px] font-bold">کد تایید را وارد کنید</h2>
+        <p className="text-[14px] text-neutralColor-2">
+          کد تایید برای شماره{user.phoneNumber} پیامک شد
+        </p>
+        <div className="flex flex-col w-full max-w-[340px]">
+          <input
+            ref={otpInputRef}
+            className="focus-within:border-2 text-neutralColor-2 w-full focus-within:border-primaryColor-1 pr-[6px] h-[48px] lg:text-[16px] border border-neutralColor-3 rounded-[5px] bg-naturalColor-2 px-7 outline-none focus:bg-naturalColor-2"
+            name="otp"
+            type="text"
+            onChange={(e) => setOtp(e.target.value)}
+            value={otp}
+            placeholder="Enter OTP"
+            autoFocus // Ensures field is focused
+          />
+        </div>
+        <button
+          type="submit"
+          className={`rounded-[5px] bg-errorColor-2 text-center px-[16px] text-naturalColor-2 text-[14px] font-medium w-full max-w-[340px] h-[48px]`}
+        >
+          {isLoading ? (
+            <ThreeDots
+              height="40"
+              width="40"
+              radius="9"
+              color="#FAFAFA"
+              ariaLabel="three-dots-loading"
+            />
+          ) : (
+            "تایید"
+          )}
+        </button>
+        <Link
+          href=""
+          onClick={() => setShowOtpPage(false)}
+          className="text-[12px] font-bold text-primaryColor-1"
+        >
+          برگشت به مرحله قبل
+        </Link>
+      </div>
+    </form>
   );
 }
